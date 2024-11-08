d19f4c95935f673e7a0d8fa8e2988484741ffd84
Andrey Tikhonov <gfouk40@gmail.com>
2024-11-08 19:14:43 +0300

    Improved homework 4

--- hw4/list04_dz/program01.cpp
+++ hw4/list04_dz/program01.cpp
@@ -7,8 +7,8 @@ int *allocate_memory(int n) {
     return (int*) malloc(n * sizeof(int));
 }
 
-int sum_products(int *a, int *b, int n) {
-    int res = 0;
+double sum_products(int *a, int *b, int n) {
+    double res = 0;
 
     for (int i = 0; i < n; i++) {
         res += a[i] * b[i];
@@ -17,8 +17,18 @@ int sum_products(int *a, int *b, int n) {
     return res;
 }
 
-int sum(int *a, int n, int power = 1) {
-    int res = 0;
+double sum(int *a, int n) {
+    double res = 0;
+
+    for (int i = 0; i < n; i++) {
+        res += a[i];
+    }
+
+    return res;
+}
+
+double power_sum(int *a, int n, int power) {
+    double res = 0;
 
     for (int i = 0; i < n; i++) {
         res += pow(a[i], power);
@@ -27,23 +37,19 @@ int sum(int *a, int n, int power = 1) {
     return res;
 }
 
-int avg(int *a, int n) {
+double avg(int *a, int n) {
     return sum(a, n) / n;
 }
 
-int linear_regression(int b0, int b1, int x) {
+double linear_regression(double b0, double b1, int x) {
     return b0 + b1 * x;
-}
-
-void copy_vector(int *a, int *b, int n) {
-    for (int i = 0; i < n; i++) {
-        b[i] = a[i];
-    }
 }
 
 
 int main() {
-    int n, b1, b0, *x, *y, *w, *z, error;
+    int n, *x, *y, new_x, new_y;
+    double b1, b0, expected_y, error;
+
     cout << "Enter n: ";
     cin >> n;
 
@@ -55,7 +61,7 @@ int main() {
         cin >> x[i] >> y[i];
     }
 
-    b1 = (sum_products(x, y, n) - n * avg(x, n) * avg(y, n)) / (sum(x, n, 2) - pow(sum(x, n), 2) / n);
+    b1 = (sum_products(x, y, n) - n * avg(x, n) * avg(y, n)) / (power_sum(x, n, 2) - pow(sum(x, n), 2) / n);
 
     b0 = avg(y, n) - b1 * avg(x, n);
 
@@ -63,26 +69,13 @@ int main() {
     for (int i = 0; i < n; i++) {
         cout << "for x = " << x[i] << ": " << linear_regression(b0, b1, x[i]) << "\n";
     }
-
-    n++;
-
-    w = allocate_memory(n);
-    copy_vector(x, w, n - 1);
-    free(x);
-
-    z = allocate_memory(n);
-    copy_vector(y, z, n - 1);
-    free(y);
     
-    cout << "Enter x & y: ";
-    cin >> w[n] >> z[n];
+    cout << "Enter new x & y: ";
+    cin >> new_x >> new_y;
 
-    b1 = (sum_products(w, z, n) - n * avg(w, n) * avg(z, n)) / (sum(w, n, 2) - pow(sum(w, n), 2) / n);
+    expected_y = linear_regression(b0, b1, new_y);
+    cout << "Linear regression for new x = " << new_x << ": " << expected_y << "\n";
 
-    b0 = avg(z, n) - b1 * avg(w, n);
-
-    cout << "Linear regression for x = " << w[n] << ": " << linear_regression(b0, b1, w[n]) << "\n";
-
-    error = abs(z[n] - linear_regression(b0, b1, w[n]));
+    error = abs(new_y - expected_y);
     cout << "Error: " << error << "\n";
 }
